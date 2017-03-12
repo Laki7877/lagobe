c0b0cb3ee4fd3a9f9df5dcbb78422ffcd86ca9e8
Laki Sik <mr.laki.sik@gmail.com>
2017-03-12 12:48:23 +0700

    Initiate application

---
+++ src/main/java/com/lagobe/DoremonApplication.java
@@ -0,0 +1,12 @@
+package com.lagobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoremonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoremonApplication.class, args);
+	}
+}
