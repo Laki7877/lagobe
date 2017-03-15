7622e16a4248a641a24d2fd8c4bea892394c1b16
Laki Sik <mr.laki.sik@gmail.com>
2017-03-15 23:40:16 +0700

    password encryption

---
+++ src/main/java/com/lagobe/utils/EncryptionUtil.java
@@ -0,0 +1,28 @@
+package com.lagobe.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EncryptionUtil {
+
+	@Value("${salt.workload}")
+	private Integer workload;
+	
+
+	public String hashPassword(String password_plaintext) {
+		String salt = BCrypt.gensalt(workload);
+		String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+		return(hashed_password);
+	}
+	
+	public boolean checkPassword(String password_plaintext, String stored_hash) {
+		boolean password_verified = false;
+		if(null == stored_hash || !stored_hash.startsWith("$2a$")) {
+			return false;
+		}
+		password_verified = BCrypt.checkpw(password_plaintext, stored_hash);
+		return password_verified;
+	}
+}
